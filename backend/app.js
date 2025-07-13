448ab2f2a8a2cc3eee091e6e233afcd0fd932833
Asadullah325 <asadullah456545@gmail.com>
2025-07-13 22:12:22 +0500

    New Changes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Define a simple route
 app.get('/', (req, res) => {
